bda2ff164994fdbd70aa82cdd1c851fa295834b3
dotun123 <justiceagunkejoye@gmail.com>
2023-05-12 13:37:04 +0100

    first commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./article.css"
+
+
+
+
+const Article = (props) => {
+  return (
+    <div className="gpt3__blog-container_article">
+   <div className="gpt3__blog-container_article-image">
+    <img src={props.imgUrl} alt="" />
+   </div>
+   <div className="gpt3__blog-container_article-content">
+    <div>
+      <p>{props.date}</p>
+      <h3>{props.title}</h3>
+    </div>
+    <p>Read full Article</p>
+   </div>
+    </div>
+  )
+}
+
+export default Article;
